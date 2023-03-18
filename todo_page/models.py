adccf0136339ea48e99bb3c6c0590cd9601b2cc8
haHAAo7 <yaroslaw1609@gmail.com>
2023-03-18 22:11:19 +0300

    added deadline field

--- todo_page/models.py
+++ todo_page/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from datetime import timedelta
+
+date_now = timezone.now()
+next_week = date_now + timedelta(days=7)
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=100, verbose_name="Название")
     details = models.TextField(verbose_name="Детали")
-    date = models.DateTimeField(default=timezone.now, verbose_name="Дата")
+    date = models.DateTimeField(default=date_now, verbose_name="Дата добавления")
+    deadline = models.DateTimeField(default=next_week, verbose_name="Дедлайн")
 
     def __str__(self):
         return self.title
