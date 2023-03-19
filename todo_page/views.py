1a3a931aa0d5cfb664e087dd9908bd81821aab41
haHAAo7 <yaroslaw1609@gmail.com>
2023-03-19 22:17:48 +0300

    added view for schedule

--- todo_page/views.py
+++ todo_page/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+import requests
+from django.http import HttpResponse
 
 from .forms import TodoForm
 from .models import Todo
@@ -28,3 +30,22 @@ def remove(request, item_id):
     messages.info(request, "Заметка удалена")
     return redirect('todo_page')
 
+
+def schedule(request):
+    current_week = requests.get("https://mirea.xyz/api/v1.3/time/week").json()
+    url = requests.get(f'https://mirea.xyz/api/v1.3/groups/certain?name=%D0%91%D0%A1%D0%9C%D0%9E-01-22')
+    data = url.json()
+    if current_week % 2:
+        week_val = "odd"
+    else:
+        week_val = "even"
+    response = f"<h2>Сейчас идет {current_week} неделя</h2><br>"
+    for day in data[0]["schedule"]:
+        response += f"<h3><b>{day['day']}</b></h3><br>"
+        for i in range(len(day[week_val])):
+            exception = ""
+            if day[week_val][i]:
+                if day[week_val][i][0]['weeks']:
+                    exception = day[week_val][i][0]['weeks']
+                response += f"{i + 1} пара {exception} - {day[week_val][i][0]['name']} - {day[week_val][i][0]['place']} - {day[week_val][i][0]['type']}<br>"
+    return render(request, 'todo_page/schedule.html', {"response": response})
